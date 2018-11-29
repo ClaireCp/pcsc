6a95760bc7622e6bdb71995515e59f0a54d0d6ab
Claire <claire.capelo@hotmail.com>
2018-11-29 15:58:20 +0100

    Continue tests without setup

--- Project/testSuite1D.cpp
+++ Project/testSuite1D.cpp
@@ -18,3 +18,20 @@ TEST(IntervalTest, reversed_intervals) {
     EXPECT_EQ(12, midPointSolver->GetFinalArgument());
 }
 
+TEST(EmptyConstructor, base_case) {
+    MidPointSolver *midPointSolver1 = new MidPointSolver;
+    midPointSolver1->SetNumberOfSteps(10);
+    midPointSolver1->SetInterval(0, 2);
+    midPointSolver1->SetFunction(function);
+    double mIntegral1 = midPointSolver1->SolveIntegral();
+    ASSERT_FLOAT_EQ(2.66, mIntegral1);
+}
+
+TEST(FullConstructor, base_case) {
+    MidPointSolver *midPointSolver2 = new MidPointSolver(10, 0, 2, function);
+    double mIntegral2 = midPointSolver2->SolveIntegral();
+    ASSERT_FLOAT_EQ(2.66, mIntegral2);
+}
+
+
+
