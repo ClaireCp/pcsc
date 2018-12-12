5f34628c32828bc0e12ae316876ce110be11b61d
Claire <claire.capelo@hotmail.com>
2018-12-12 13:39:19 +0100

    Added comments for tools functions

--- Project/Tools.hpp
+++ Project/Tools.hpp
@@ -8,8 +8,18 @@
 #ifndef TOOLS_HPP
 #define TOOLS_HPP
 
+/** Tool functions to perform checks on the arguments of numerical solvers.
+ */
+
+/** Checks that the number of steps is stricly superior to zero. If not interrupts the program.
+ * @param n number of steps
+ */
 void checkNumberOfSteps(int n);
 
+/** Checks if the initial argument is smaller than the final argument. If not, it switches them.
+ * @param x0 initial argument of the interval
+ * @param xf final argument of the interval
+ */
 void checkInterval(double* x0, double* xf);
 
 #endif // TOOLS_HPP
